527cd1006269df81753ddcdcbc9e41be6ab9c585
Esteve Fernandez <esteve@apache.org>
2022-09-15 20:55:37 +0200

    [humble] Backport. Expand timestamps for std_msgs.msg.Header and builtin_interfaces.msg.Time if 'auto' and 'now' are passed as values (#20)
    * Expand timestamps for std_msgs.msg.Header and builtin_interfaces.msg.Time if 'auto' and 'now' are passed as values
    
    Signed-off-by: Esteve Fernandez <esteve.fernandez@tier4.jp>
    
    * Added tests from https://github.com/ros2/ros2cli/pull/749
    
    Signed-off-by: Esteve Fernandez <esteve.fernandez@tier4.jp>
    
    * Added tests for unparsed now and auto keywords
    
    Signed-off-by: Esteve Fernandez <esteve.fernandez@tier4.jp>
    
    * Fix flake8 error
    
    Signed-off-by: Esteve Fernandez <esteve.fernandez@tier4.jp>
    
    Signed-off-by: Esteve Fernandez <esteve.fernandez@tier4.jp>

--- test/rosidl_runtime_py/test_set_message.py
+++ test/rosidl_runtime_py/test_set_message.py
@@ -12,12 +12,79 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import builtins
 import copy
 
+from builtin_interfaces.msg import Time
 import pytest
-
+import rosidl_parser.definition
 from rosidl_runtime_py import set_message_fields
+from std_msgs.msg import Header
 from test_msgs import message_fixtures
+
+
+class MockMessageStamped:
+
+    __slots__ = [
+        '_header',
+    ]
+
+    _fields_and_field_types = {
+        'header': 'std_msgs/Header',
+    }
+
+    SLOT_TYPES = (
+        rosidl_parser.definition.NamespacedType(['std_msgs', 'msg'], 'Header'),
+    )
+
+    def __init__(self):
+        self.header = Header()
+
+    @builtins.property
+    def header(self):
+        return self._header
+
+    @header.setter
+    def header(self, value):
+        self._header = value
+
+
+class MockMessageWithStampFields:
+
+    __slots__ = [
+        '_timestamp1',
+        '_timestamp2',
+    ]
+
+    _fields_and_field_types = {
+        'timestamp1': 'builtin_interfaces/Time',
+        'timestamp2': 'builtin_interfaces/Time',
+    }
+
+    SLOT_TYPES = (
+        rosidl_parser.definition.NamespacedType(['builtin_interfaces', 'msg'], 'Time'),
+        rosidl_parser.definition.NamespacedType(['builtin_interfaces', 'msg'], 'Time'),
+    )
+
+    def __init__(self):
+        self.timestamp1 = Time()
+        self.timestamp2 = Time()
+
+    @builtins.property
+    def timestamp1(self):
+        return self._timestamp1
+
+    @timestamp1.setter
+    def timestamp1(self, value):
+        self._timestamp1 = value
+
+    @builtins.property
+    def timestamp2(self):
+        return self._timestamp2
+
+    @timestamp2.setter
+    def timestamp2(self, value):
+        self._timestamp2 = value
 
 
 def test_set_message_fields_none():
@@ -148,3 +215,83 @@ def test_set_message_fields_nested_type():
     set_message_fields(msg0, test_values)
 
     assert msg0.basic_types_value == msg_basic_types
+
+
+def test_set_message_fields_header_auto():
+    msg = MockMessageStamped()
+    values = {'header': 'auto'}
+    assert msg.header.stamp.sec == 0 and msg.header.stamp.nanosec == 0
+    assert msg.header.frame_id == ''
+    timestamp_fields = set_message_fields(
+        msg, values, expand_header_auto=True, expand_time_now=True)
+    assert timestamp_fields is not None
+    for field_setter in timestamp_fields:
+        stamp = Time(sec=1, nanosec=2)
+        field_setter(stamp)
+    assert msg.header.stamp.sec == 1 and msg.header.stamp.nanosec == 2
+    assert msg.header.frame_id == ''
+
+
+def test_set_message_fields_header_auto_not_parsed():
+    msg = MockMessageStamped()
+    values = {'header': 'auto'}
+    assert msg.header.stamp.sec == 0 and msg.header.stamp.nanosec == 0
+    assert msg.header.frame_id == ''
+    with pytest.raises(
+            TypeError, match=r"^Value 'auto' is expected to be a dictionary but is a str$"):
+        set_message_fields(msg, values)
+
+
+def test_set_message_fields_stamp_now():
+    msg = MockMessageStamped()
+    values = {'header': {'stamp': 'now'}}
+    assert msg.header.stamp.sec == 0 and msg.header.stamp.nanosec == 0
+    assert msg.header.frame_id == ''
+    timestamp_fields = set_message_fields(
+        msg, values, expand_header_auto=True, expand_time_now=True)
+    assert timestamp_fields is not None
+    for field_setter in timestamp_fields:
+        stamp = Time(sec=1, nanosec=2)
+        field_setter(stamp)
+    assert msg.header.stamp.sec == 1 and msg.header.stamp.nanosec == 2
+    assert msg.header.frame_id == ''
+
+
+def test_set_message_fields_stamp_now_not_parsed():
+    msg = MockMessageStamped()
+    values = {'header': {'stamp': 'now'}}
+    assert msg.header.stamp.sec == 0 and msg.header.stamp.nanosec == 0
+    assert msg.header.frame_id == ''
+    with pytest.raises(
+            TypeError, match=r"^Value 'now' is expected to be a dictionary but is a str$"):
+        set_message_fields(msg, values)
+
+
+def test_set_message_fields_stamp_now_with_frame_id():
+    msg = MockMessageStamped()
+    values = {'header': {'stamp': 'now', 'frame_id': 'hello'}}
+    assert msg.header.stamp.sec == 0 and msg.header.stamp.nanosec == 0
+    assert msg.header.frame_id == ''
+    timestamp_fields = set_message_fields(
+        msg, values, expand_header_auto=True, expand_time_now=True)
+    assert timestamp_fields is not None
+    for field_setter in timestamp_fields:
+        stamp = Time(sec=1, nanosec=2)
+        field_setter(stamp)
+    assert msg.header.stamp.sec == 1 and msg.header.stamp.nanosec == 2
+    assert msg.header.frame_id == 'hello'
+
+
+def test_set_message_fields_stamp_now_with_timestamp_fields():
+    msg = MockMessageWithStampFields()
+    values = {'timestamp1': 'now', 'timestamp2': 'now'}
+    assert msg.timestamp1.sec == 0 and msg.timestamp1.nanosec == 0
+    assert msg.timestamp2.sec == 0 and msg.timestamp2.nanosec == 0
+    timestamp_fields = set_message_fields(
+        msg, values, expand_header_auto=True, expand_time_now=True)
+    assert timestamp_fields is not None
+    for field_setter in timestamp_fields:
+        stamp = Time(sec=1, nanosec=2)
+        field_setter(stamp)
+    assert msg.timestamp1.sec == 1 and msg.timestamp1.nanosec == 2
+    assert msg.timestamp2.sec == 1 and msg.timestamp2.nanosec == 2
